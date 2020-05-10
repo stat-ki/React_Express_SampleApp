f7ea695dfc3e8c8c2e4c4629332a92bd5bff1d9e
stat-ki <per_ik@outlook.com>
2020-05-10 10:43:37 +0000

    Add authorize function

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 // define the Express app
 const app = express();
@@ -43,20 +45,21 @@ app.get('/:id', (req, res) => {
 });
 
 // insert a new question
-app.post('/', (req, res) => {
+app.post('/', checkJwt, (req, res) => {
   const {title, description} = req.body;
   const newQuestion = {
     id: questions.length + 1,
     title,
     description,
     answers: [],
+    author: req.user.name,
   };
   questions.push(newQuestion);
   res.status(200).send();
 });
 
 // insert a new answer to a question
-app.post('/answer/:id', (req, res) => {
+app.post('/answer/:id', checkJwt, (req, res) => {
   const {answer} = req.body;
 
   const question = questions.filter(q => (q.id === parseInt(req.params.id)));
@@ -65,9 +68,24 @@ app.post('/answer/:id', (req, res) => {
 
   question[0].answers.push({
     answer,
+    author: req.user.name,
   });
 
   res.status(200).send();
+});
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-2-90xc42.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: '1ygYWxEIs2ejc8u1aiRoZ9avo2ypElE9',
+  issuer: `https://dev-2-90xc42.auth0.com`,
+  algorithms: ['RS256']
 });
 
 // start the server
